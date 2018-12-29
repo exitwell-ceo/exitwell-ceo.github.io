7d34c402d6e645319b7e717979881715395103d8
exitwell-ceo <exitwell-ceo@github.com>
2018-12-30 00:11:49 +0900

    Update like_button.js

--- like_button.js
+++ like_button.js
@@ -8,10 +8,10 @@ class LikeButton extends React.Component {
     super(props);
     this.state = { liked: false };
   }
-
+// <div>(12/26) *홍* : - <br> (12/26) **상 : - <br> (12/27) *우* : -\n</div>
   render() {
     if (this.state.liked) {
-      return ( <div>(12/26) *홍* : - <br> (12/26) **상 : - <br> (12/27) *우* : -\n</div>);
+      return '김홍홍 : 가즈아 희생 : 형 벤모 돼요? *우* : 놀러감';
     }
 
     return e(
