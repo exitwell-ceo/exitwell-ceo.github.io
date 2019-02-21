fd0a59a2a96c6edfea8252fd5a0fb3c805b0ecae
exitwell-ceo <46244917+exitwell-ceo@users.noreply.github.com>
2019-02-21 22:09:22 +0900

    updated new web

---
+++ precache-manifest.2c3f3982d597a031beb312edac08000d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "36bb87669612075fe491",
+    "url": "/static/js/main.36bb8766.chunk.js"
+  },
+  {
+    "revision": "27bf3c76d6c72d89b061",
+    "url": "/static/js/2.27bf3c76.chunk.js"
+  },
+  {
+    "revision": "36bb87669612075fe491",
+    "url": "/static/css/main.d1532b3f.chunk.css"
+  },
+  {
+    "revision": "d8355952a08bd774f1c376ea0143d39f",
+    "url": "/index.html"
+  }
+];
